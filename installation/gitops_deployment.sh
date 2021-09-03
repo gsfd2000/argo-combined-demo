e6037c38510dc872d6ab285d0d3ad86cb37d3846
gsfd2000 <gsfd2000@gmail.com>
2021-09-03 21:49:18 +0200

    rerun env variable set for root account via source ./replace_env.sh

---
+++ installation/gitops_deployment.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+######################
+# GitOps deployments #
+######################
+
+# cd argo-combined-demo
+
+ls -1 ../production/
+
+cat ../production/argo-cd.yaml
+
+kustomize build \
+    ../argo-cd/overlays/production \
+    | kubectl apply --filename -
+
+kubectl --namespace argocd \
+    rollout status \
+    deployment argocd-server
+
+export PASS=$(kubectl \
+    --namespace argocd \
+    get secret argocd-initial-admin-secret \
+    --output jsonpath="{.data.password}" \
+    | base64 --decode)
+
+argocd login \
+    --insecure \
+    --username admin \
+    --password $PASS \
+    --grpc-web \
+    argo-cd.$BASE_HOST
+
+argocd account update-password \
+    --current-password $PASS \
+    --new-password admin
+
+xdg-open http://argo-cd.$BASE_HOST
+
+# Both user and password are `admin`
+
+cat ../project.yaml
+
+kubectl apply --filename ../project.yaml
+
+cat ../apps.yaml
+
+kubectl apply --filename ../apps.yaml
